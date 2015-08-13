65619e824d17e53cc4abc3af689d6aea7fd4db57
Nathan Kozyra <nkozyra@gmail.com>
2015-08-13 12:22:48 -0400

    Callbacks, exists and cleanup

--- src/pagu.js
+++ src/pagu.js
@@ -5,48 +5,76 @@ var Pagu = function(obj,template) {
   self.obj = obj;
   self.template = template;
   self.parsed = template;
+  this.escapeRegexp = function(r) {
+    return new RegExp(r.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"),'gm');
+  };
   this.Pagu = function(tplObject,tplOverride) {
     var obj = (tplObject ? tplObject : self.obj);
     var parsed = ( tplOverride ? tplOverride : self.parsed );
-    // console.log(obj)
+
     for (var o in obj) {
+      if (obj.hasOwnProperty(o)) {
 
-      //  Value comparisons
-      var isTestPat = new RegExp("{{"+o+"\\.(is|!is)\\((.*?)\\)}}(.*?){{/"+o+"\\.is}}",'m');
-      var valComp = isTestPat.exec(parsed);
-      while (valComp != null) {
-        var cmp = (valComp[1] == '!is' ? false : true);
-
-        var innerTest = valComp[2];
-        var removePat = new RegExp(valComp[0].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"),'gm');
-        var replaceString = '';
-        if ((innerTest == obj[o]) === cmp) {
-          replaceString = valComp[3];
+        // objcts
+        if (obj[o] && {}.toString.call(obj[o]) === '[object Function]') {
+         obj[o] = obj[o]();
         }
-        var foo = new RegExp("{{name.is(2)}}valid int!{{\/name.is}}",'gm');
-        parsed = parsed.replace(removePat,replaceString);
-        valComp = isTestPat.exec(parsed);
-      }
 
-      var tplVar = new RegExp("{{"+o+"}}", 'gi');
-      parsed = parsed.replace(tplVar, obj[o]);
-
-      if (obj[o] instanceof Array) {
-        var fullTpl = "{{"+o+"\.each}}(.*?){{/"+o+"\.each}}";
-        var totTpl = ".*?" + fullTpl + ".*";
-        var arrTplVar = new RegExp(fullTpl,'gim');
-        var arrTotTpl = new RegExp(totTpl, 'gim');
-        var innerTemplate = parsed.replace(arrTotTpl,'$1');
-        var replacement = '';
-        for (var i=0;i<obj[o].length;i++) {
-          replacement += self.Pagu(obj[o][i], innerTemplate);
+        //  Has value : {{var.if}}has value{{/var.if}}
+        var ifTestPat = new RegExp("{{"+o+"\\.if}}(.*?){{/"+o+"\\.if}}","gm");
+        var ifComp = ifTestPat.exec(parsed);
+        while (ifComp != null) {
+          var inner = ifComp[1];
+          var remove = self.escapeRegexp(ifComp[0]);
+          if (obj[o]) {
+            parsed = parsed.replace(remove,inner);
+          } else {
+            parsed = parsed.replace(remove,'');
+          }
+          ifComp = ifTestPat.exec(parsed);
         }
-        parsed = parsed.replace(arrTplVar,replacement);
+
+        //  Value comparisons
+        var isTestPat = new RegExp("{{"+o+"\\.(is|!is)\\((.*?)\\)}}(.*?){{/"+o+"\\.(\\1)}}","gm"); console.log('pat: ' + isTestPat);  
+        var valComp = isTestPat.exec(parsed);
+        while (valComp != null) {
+          var cmp = (valComp[1] === '!is' ? false : true);
+          var innerTest = valComp[2];
+          var removePat = new RegExp(valComp[0].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"),'gm');
+          console.log(removePat);
+          var replaceString = '';
+          if ((innerTest == obj[o]) === cmp) {
+            replaceString = valComp[3];
+          }
+          parsed = parsed.replace(removePat,replaceString);
+          valComp = isTestPat.exec(parsed);
+        }
+
+        var tplVar = new RegExp("{{"+o+"}}", 'gi');
+        parsed = parsed.replace(tplVar, obj[o]);
+
+
+        if (obj[o] instanceof Array) {
+          var fullTpl = "{{"+o+"\.each}}(.*?){{/"+o+"\.each}}";
+          var totTpl = ".*?" + fullTpl + ".*";
+          var arrTplVar = new RegExp(fullTpl,'gim');
+          var arrTotTpl = new RegExp(totTpl, 'gim');
+          var innerTemplate = parsed.replace(arrTotTpl,'$1');
+          var replacement = '';
+          for (var i=0;i<obj[o].length;i++) {
+            replacement += self.Pagu(obj[o][i], innerTemplate);
+          }
+          parsed = parsed.replace(arrTplVar,replacement);
+        }
       }
-    };
+    }
+    var cleanupRegexp = new RegExp("{{([A-Za-z_0-9])+\..*?}}(.*?){{\/\\1\.*?}}","gm");
+    parsed = parsed.replace(cleanupRegexp,'');
+    var cleanUnsetVals = new RegExp("{{([A-Za-z_0-9])+}}","gm");
+    parsed = parsed.replace(cleanUnsetVals, '');
     self.parsed = parsed;
     return self.parsed;
-  }
+  };
   this.parse = function() {
     return self.parsed;
   };
